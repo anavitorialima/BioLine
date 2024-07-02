e446b9df3b530951f4e03b310f999bbd21fab275
KleberManoel <klebermanoel703@gmail.com>
2024-07-02 16:35:07 -0300

    concluiu login, logout e estado de atencidade

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,27 +1,86 @@
 import { auth } from './firebase.js'
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
 
 document.addEventListener('DOMContentLoaded', () => {
     const formLogin = document.querySelector('.form-login')
     const emailInput = document.querySelector('.email-admin')
     const senhaInput = document.querySelector('.senha-admin')
+    const linkSair = document.querySelector('.link-sair')
+    const formPost = document.querySelector('.form-post')
+    const titulologin = document.querySelector('.titulo-login')
+    const mensagens = document.querySelector('.mensagens')
 
-    formLogin.addEventListener('submit', (e) => {
-        e.preventDefault()
-        const email = emailInput.value
-        const senha = senhaInput.value
+    // mensagens
+    const logadoSucesso = () =>{
+        mensagens.innerHTML = 'Usuário logado com sucesso.'
+    }
 
-        if(formLogin){
-            signInWithEmailAndPassword(auth, email, senha)
-            .then((userCredential) => {
-                const user = userCredential.user
-                alert('Usuário logado com sucesso')
-                emailInput.value = ''
-                senhaInput.value = ''
-            })
-            .catch(()=>{
-                alert('Verifique a sua conexão ou o seu email e senha.')
-            })
+    const verifiqueConexao = () =>{
+        mensagens.innerHTML = 'Verifique a sua conexão ou o seu email e senha.'
+    }
+
+    const logoutSucesso = () => {
+        mensagens.innerHTML = 'Logout realizado com sucesso.'
+    }
+
+    const erroInesperado = () => {
+        mensagens.innerHTML = 'Ocorreu um erro Inesperado.'
+    }
+
+    if (formLogin) {
+        formLogin.addEventListener('submit', (e) => {
+            e.preventDefault()
+            const email = emailInput.value
+            const senha = senhaInput.value
+
+            if (formLogin) {
+                signInWithEmailAndPassword(auth, email, senha)
+                    .then((userCredential) => {
+                        const user = userCredential.user
+                        alert('Usuário logado com sucesso')
+                        emailInput.value = ''
+                        senhaInput.value = ''
+                    })
+                    .catch(() => {
+                       verifiqueConexao()
+                    })
+            }
+
+        })
+    }
+
+    if (linkSair) {
+        linkSair.addEventListener('click', () => {
+            signOut(auth)
+                .then(() => {
+                    logoutSucesso()
+                })
+                .catch(() => {
+                    erroInesperado()
+                })
+
+        })
+    }
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            const uid = user.uid
+
+            if (linkSair) linkSair.classList.remove('hide')
+            if (formLogin) formLogin.classList.add('hide')
+            if (formPost) formPost.classList.remove('hide')
+            if (titulologin) titulologin.innerHTML = 'Acesso Liberado.'
+        } else {
+            if (linkSair) linkSair.classList.add('hide')
+            if (formLogin) formLogin.classList.remove('hide')
+            if (formPost) formPost.classList.add('hide')
+            if (titulologin) titulologin.innerHTML = 'FAÇA O LOGIN NO SITE!!'
         }
+
     })
+
+
+
+
 })
+
--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -3,14 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiIVLic_s18atZtBaJmTSNDVjqO_7u1FU",
-  authDomain: "turmaterca-d2878.firebaseapp.com",
-  databaseURL: "https://turmaterca-d2878-default-rtdb.firebaseio.com",
-  projectId: "turmaterca-d2878",
-  storageBucket: "turmaterca-d2878.appspot.com",
-  messagingSenderId: "179178679657",
-  appId: "1:179178679657:web:ad5cb958aec320facc21f8",
-  measurementId: "G-492TNV85B3"
+  apiKey: "AIzaSyAm-OpTUE8Az5yeyoORb8fjiNrhpH5sbQQ",
+  authDomain: "tcc-3a.firebaseapp.com",
+  projectId: "tcc-3a",
+  storageBucket: "tcc-3a.appspot.com",
+  messagingSenderId: "287360685081",
+  appId: "1:287360685081:web:4f60129ffa80540695c688",
+  measurementId: "G-49EXPN0JX6"
 };
 
 const app = initializeApp(firebaseConfig);
