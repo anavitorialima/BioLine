d6d4966ae4b16ddd71491a1d95752596293e4173
anavitorialima <anavitorialima006h@gmail.com>
2024-08-10 11:47:08 -0300

    Listagem de gerenciador de conteúdos

--- assets/js/database.js
+++ assets/js/database.js
@@ -1,24 +1,25 @@
 import { database, storage } from "./firebase.js";
-//as significa um apelido ou seja um databaseRef e o apelido de ref que é uma função do firebase
-import { set, ref as databaseRef, onValue } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
-import { ref as storageref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";
+// as significa um apelido ou seja databaseRef é o apelido de ref que é uma função do firebase
+import { set, ref as databaseRef, onValue, remove } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js"
+import { ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js"
 
 document.addEventListener('DOMContentLoaded', () => {
-    //Declarando variáveis
+    // Declarando as variáveis
     const formPost = document.querySelector('.form-post')
     const tituloPost = document.querySelector('.titulo-post')
-    const sendPost = document.querySelector('.send-post')
     const imagemPost = document.querySelector('.imagem-post')
     const mensagemPost = document.querySelector('.mensagem-post')
     const dataPublicacaoPost = document.querySelector('.data-publicacao-post')
     const autorPost = document.querySelector('.autor-post')
-    let divConteudos = document.querySelector('.conteudos')
+    const sendPost = document.querySelector('.send-post')
+    const divConteudos = document.querySelector('.conteudos')
+    const gerenciadorConteudos = document.querySelector('.gerenciador-conteudos')
 
-    const postsref = databaseRef(database, 'posts')
+    const postsRef = databaseRef(database, `posts`)
 
     if (sendPost && tituloPost && mensagemPost && dataPublicacaoPost && autorPost && imagemPost) {
-
-        //Grava as informações
+        
+        // Grava as informações
         const enviarPost = (postId, titulo, mensagem, data, autor, imagemUrl) => {
             return set(databaseRef(database, `posts/${postId}`), {
                 titulo,
@@ -28,9 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 imagemUrl
             })
         }
+
         //Envia os dados gravados
         sendPost.addEventListener('click', () => {
-            const postId = new Date().getTime().toString()
+            const postId = new Date().getTime().toString() // Um identificador baseado em milisegundos
             const titulo = tituloPost.value
             const mensagem = mensagemPost.value
             const data = dataPublicacaoPost.value
@@ -38,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const imagem = imagemPost.files[0]
 
             if (imagem) {
-                const imagemRef = storageref(storage, `posts/${postId}/${imagem.name}`)
+                const imagemRef = storageRef(storage, `posts/${postId}/${imagem.name}`)
                 uploadBytes(imagemRef, imagem)
                     .then((snapshot) => {
                         getDownloadURL(snapshot.ref)
@@ -55,7 +57,6 @@ document.addEventListener('DOMContentLoaded', () => {
                                         console.log(error)
                                     })
                             })
-
                     })
             }
 
@@ -63,28 +64,84 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const listarPosts = (conteudos) =>{
-        onValue(postsref,(snapshot) =>{
+        onValue(postsRef,(snapshot)=>{
             const posts = snapshot.val()
-            divConteudos.innerHTML = ''
+            if( divConteudos){
+                divConteudos.innerHTML = ''
+            }
+            
             if(posts){
-                const postIds = Object.keys(posts)
-                postIds.forEach(postId =>{
+                const postsIds = Object.keys(posts).sort((a,b)=>b-a)
+                postsIds.forEach((postId) =>{
                     const post = posts[postId]
                     const postElement = document.createElement('div')
-                    postElement.innerHTML =`
-                       <h2 class"mt-5 fw-bold text-center text-success"> ${post.titulo}</h2>
-                       <div class"decoration-bar" ></div>
+                    postElement.innerHTML = `
+                        <h2 class="mt-5 fw-bold text-center text-success" >${post.titulo}</h2>
+                        <div class="decoration-bar" ></div>
+                        <img src="${post.imagemUrl}" alt="imagem de ${post.titulo}" class="img-blog my-5 img-fluid" />
+                        <p>${post.mensagem}</p>
+                        <p class="align-self-center mt-5" >Publicado em:${post.data} por ${post.autor}.</p>
+                        <hr/>
                     `
-                    divConteudos.appendChild(postElement)
+                    if( divConteudos){
+                        divConteudos.appendChild(postElement)
+                    }
+                   
                 })
             }else{
-                divConteudos.innerHTML='<p class="mt-5" >Nenhum Post encontardo</p>'
+                if(divConteudos){
+                    divConteudos.innerHTML='<p class="mt-5" >Nenhum post encontrado.</p>' 
+                }
             }
-
         })
-
     }
 
-    listarPosts()
+    const gerenciarPosts = (conteudos) =>{
+        onValue(postsRef,(snapshot)=>{
+            const posts = snapshot.val()
+            conteudos.innerHTML = ''
+            if(posts){
+                const postIds = Object.keys(posts).sort((a,b)=>b-a)
+                postIds.forEach((postId)=>{
+                    const post = posts[postId]
+                    const postElement = document.createElement('div')
+                    postElement.innerHTML = `
+                        <button class="btn btn-danger btn-sm mx-3 delete-post" data-id="${post.titulo}"><i class="bi bi-trash3-fill"></i>Apagar</button>
+                        <span class="fw-bold" >${post.titulo}</span>
+                        <hr/>
+                    `
+                    conteudos.appendChild(postElement)
+                })
+                document.querySelectorAll('.delete-post').forEach((button)=>{
+                    button.addEventListener('click',(e)=>{
+                        const postId = e.target.getAtribute('data-id')
+                        apagarPost(postId)
+                    })
+                })
+            }else{
+                conteudos.innerHTML = '<p class="mt-5" >Nenhum post encontrado.</p>'
+            }
+        })
+    }
 
+    const apagarPost = (postId)=>{
+         remove(databaseRef(database,`posts/${postId}`)).then(()=>{
+            alert('Post removido com sucesso')
+            listarPosts(divConteudos)
+            gerenciarPosts(gerenciadorConteudos)
+         })
+         .catch((error)=>{
+            console.log(`Erro ao tentar apagar o post ${error}`)
+            alert('Erro ao tentar remove o post')
+         })
+    }
+
+    if(divConteudos){
+        listarPosts(divConteudos)
+    }
+    if(gerenciadorConteudos){
+        gerenciarPosts(gerenciadorConteudos)
+    }
+    
+    
 })
